2cd6a466bd1b9fa8b8d7e7c420923c073837c70f
David Carlyn <dcarlyn@kent.edu>
2017-12-31 12:15:53 -0500

    Added meta tags to allow mobile compatability

--- login/login.php
+++ login/login.php
@@ -62,6 +62,8 @@
 <html>
 	<head>
 		<title>Phi Kappa Psi Login</title>
+		<meta charset="utf-8">
+  		<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
 
 	<style>
--- login/register.php
+++ login/register.php
@@ -113,6 +113,8 @@
 <html>
 	<head>
 		<title>Phi Kappa Psi Register</title>
+		<meta charset="utf-8">
+  		<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
 
 	<style>
